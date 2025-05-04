a7b5099a235e7cec2bf38c4e074714eafca311c9
Java-rice <johnmark.p.peroche@gmail.com>
2025-05-04 16:37:15 +0800

    feat: create content

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,28 @@ import {
 } from "react-router-dom";
 import MainLayout from "@src/layout/MainLayout";
 import Home from "@src/pages/Home/Home";
+import About from "@src/pages/About/About";
+
+import Learn from "@src/pages/Learn/Learn";
+import Materials from "@src/pages/Learn/Materials";
+import Course from "@src/pages/Learn/Course";
+import Play from "@src/pages/Learn/Play";
+
+import Community from "@src/pages/Community/Community";
+import Activities from "@src/pages/Community/Activities";
+import Partners from "@src/pages/Community/Partners";
+import Social from "@src/pages/Community/Social";
+
+import Blog from "@src/pages/Blogs/Blog"
+import Blogs from "@src/pages/Blogs/Blogs";
+import News from "@src/pages/Blogs/News";
+import NewsLetters from "@src/pages/Blogs/NewsLetters";
+
+import Contacts from "@src/pages/Contacts/Contacts";
+import Partner from "@src/pages/Contacts/Partner";
+import Report from "@src/pages/Contacts/Report";
+import Feedback from "@src/pages/Contacts/Feedback";
+
 function App() {
   return (
     <>
@@ -17,29 +39,28 @@ function App() {
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/learn">
-              <Route path="/" element={<About/>}/>
-              <Route path="/learning-materials" element={<About/>}/>
-              <Route path="/crash-course" element={<About/>}/>
-              <Route path="/play" element={<About/>}/>
+              <Route index element={<Learn/>}/>
+              <Route path="learning-materials" element={<Materials/>}/>
+              <Route path="crash-course" element={<Course/>}/>
+              <Route path="play" element={<Play/>}/>
             </Route>
             <Route path="/community">
-              <Route path="/" element={<About/>}/>
-              <Route path="/activities" element={<About/>}/>
-              <Route path="/partners" element={<About/>}/>
-              <Route path="/socials" element={<About/>}/>
+              <Route index element={<Community/>}/>
+              <Route path="activities" element={<Activities/>}/>
+              <Route path="partners" element={<Partners/>}/>
+              <Route path="socials" element={<Social/>}/>
             </Route>
-            <Route path="/blogs">
-              <Route path="/" element={<About/>}/>
-              <Route path="/news" element={<About/>}/>
-              <Route path="/blogs" element={<About/>}/>
-              <Route path="/newsletter" element={<About/>}/>
+            <Route path="/blog">
+              <Route index element={<Blog/>}/>
+              <Route path="news" element={<News/>}/>
+              <Route path="blogs" element={<Blogs/>}/>
+              <Route path="newsletter" element={<NewsLetters/>}/>
             </Route>
             <Route path="/contacts">
-              <Route path="/" element={<About/>}/>
-              <Route path="/contact" element={<About/>}/>
-              <Route path="/partner" element={<About/>}/>
-              <Route path="/report" element={<About/>}/>
-              <Route path="/feedback" element={<About/>}/>
+              <Route index element={<Contacts/>}/>
+              <Route path="partner" element={<Partner/>}/>
+              <Route path="report" element={<Report/>}/>
+              <Route path="feedback" element={<Feedback/>}/>
             </Route>
 
           </Route>
